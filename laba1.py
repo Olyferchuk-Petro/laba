c94e1ff9ae33d3eafbf1aa06f9d4e7da5fd5df25
Olyferchuk-Petro <215143ua@gmail.com>
2026-09-07 22:12:20 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,32 @@
+
+print("Hello, World!")
+
+age = 16  # int
+pi = 3.14  # float
+text = "Лабораторна робота"  # str
+is_student = True  # bool
+
+lst = [1, 2, 3, 4, 5]  # list
+tpl = (1, 2, 3, 4)  # tuple
+st = {1, 2, 3, 4, 5}  # set
+my_dict = {"name": "Petro", "group": "11-KB"}  # dict
+
+print(age, type(age))
+print(pi, type(pi))
+print(text, type(text))
+print(is_student, type(is_student))
+print(lst, type(lst))
+print(tpl, type(tpl))
+print(st, type(st))
+print(my_dict, type(my_dict))
+
+x = 15
+y = 4
+
+print("Додавання (+):", x + y)
+print("Віднімання (-):", x - y)
+print("Множення (*):", x * y)
+print("Ділення (/):", x / y)
+print("Остача (%):", x % y)
+print("Цілочисельне ділення (//):", x // y)
+print("Степінь (**):", x**y)
